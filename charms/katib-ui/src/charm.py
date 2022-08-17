3460c7c3f6c900d50bef27f182cb2faf835aa4ed
misohu <michal.hucko@canonical.com>
2022-08-17 15:08:25 +0200

    fix: use static sibar link spec + send list of links

--- charms/katib-ui/src/charm.py
+++ charms/katib-ui/src/charm.py
@@ -154,13 +154,15 @@ class Operator(CharmBase):
         event.relation.data[self.app].update(
             {
                 "config": json.dumps(
-                    {
-                        "app": self.app.name,
-                        "type": "item",
-                        "link": self.model.config["sidebar-link"],
-                        "text": self.model.config["sidebar-text"],
-                        "icon": self.model.config["sidebar-icon"],
-                    }
+                    [
+                        {
+                            "app": self.app.name,
+                            "type": "item",
+                            "link": "/katib/",
+                            "text": "Experiments (AutoML)",
+                            "icon": "kubeflow:katib",
+                        }
+                    ]
                 )
             }
         )
