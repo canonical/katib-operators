8e8fc55e262f161ff7cb215c5bea2dd3b7ad8586
misohu <michal.hucko@canonical.com>
2022-08-23 13:21:56 +0200

    feat: remove relation data on departed

--- charms/katib-ui/src/charm.py
+++ charms/katib-ui/src/charm.py
@@ -42,6 +42,10 @@ class Operator(CharmBase):
         self.framework.observe(
             self.on.sidebar_relation_joined, self._on_sidebar_relation_joined
         )
+        self.framework.observe(
+            self.on.sidebar_relation_departed,
+            self._on_sidebar_relation_departed,
+        )
 
     def set_pod_spec(self, event):
         try:
@@ -157,6 +161,7 @@ class Operator(CharmBase):
                     [
                         {
                             "app": self.app.name,
+                            "position": 5,
                             "type": "item",
                             "link": "/katib/",
                             "text": "Experiments (AutoML)",
@@ -166,6 +171,11 @@ class Operator(CharmBase):
                 )
             }
         )
+
+    def _on_sidebar_relation_departed(self, event):
+        if not self.unit.is_leader():
+            return
+        event.relation.data[self.app].update({"config": json.dumps([])})
 
 
 if __name__ == "__main__":
