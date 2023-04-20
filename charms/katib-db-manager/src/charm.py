b95db83ede662591d5dd288b1e6b37fb44d86655
i-chvets <113444075+i-chvets@users.noreply.github.com>
2023-04-20 08:36:45 -0400

    feat: mysql-k8s integration (#72)
    * feat: mysql-k8s integration
    https://github.com/canonical/katib-operators/issues/74
    
    Summary of changes:
    - Initial implementation and integration test.
    - Unit test are passing.
    - Integration test are not passing.
    - Making it a draft PR.
    - Refactored code according to comments.
    - Added complete integration test.
    - Shortened error messages.
    - Added proper BlockedStatus skip in update status.

--- charms/katib-db-manager/src/charm.py
+++ charms/katib-db-manager/src/charm.py
@@ -8,18 +8,21 @@ from charmed_kubeflow_chisme.exceptions import ErrorWithStatus, GenericCharmRunt
 from charmed_kubeflow_chisme.kubernetes import KubernetesResourceHandler
 from charmed_kubeflow_chisme.lightkube.batch import delete_many
 from charmed_kubeflow_chisme.pebble import update_layer
+from charms.data_platform_libs.v0.data_interfaces import DatabaseRequires
 from charms.observability_libs.v1.kubernetes_service_patch import KubernetesServicePatch
 from lightkube import ApiError
 from lightkube.generic_resource import load_in_cluster_generic_resources
 from lightkube.models.core_v1 import ServicePort
 from ops.charm import CharmBase
 from ops.main import main
-from ops.model import ActiveStatus, MaintenanceStatus, ModelError, WaitingStatus
+from ops.model import ActiveStatus, BlockedStatus, MaintenanceStatus, ModelError, WaitingStatus
 from ops.pebble import CheckStatus, Layer
 
 K8S_RESOURCE_FILES = [
     "src/templates/auth_manifests.yaml.j2",
 ]
+MYSQL_WARNING = "Relation mysql is deprecated."
+UNBLOCK_MESSAGE = "Remove deprecated mysql relation to unblock."
 
 
 class KatibDBManagerOperator(CharmBase):
@@ -31,6 +34,7 @@ class KatibDBManagerOperator(CharmBase):
         # retrieve configuration and base settings
         self.logger = logging.getLogger(__name__)
         self._container_name = "katib-db-manager"
+        self._database_name = "mysql"
         self._container = self.unit.get_container(self._container_name)
         self._exec_command = "./katib-db-manager"
         self._port = self.model.config["port"]
@@ -38,11 +42,9 @@ class KatibDBManagerOperator(CharmBase):
         self._namespace = self.model.name
         self._name = self.model.app.name
         self._k8s_resource_handler = None
-        self._mysql_data = None
+        self._db_data = None
 
         # setup events to be handled by main event handler
-        self.framework.observe(self.on["mysql"].relation_joined, self._on_event)
-        self.framework.observe(self.on["mysql"].relation_changed, self._on_event)
         self.framework.observe(self.on.katib_db_manager_pebble_ready, self._on_event)
         self.framework.observe(self.on.config_changed, self._on_event)
 
@@ -50,6 +52,29 @@ class KatibDBManagerOperator(CharmBase):
         self.framework.observe(self.on.install, self._on_install)
         self.framework.observe(self.on.remove, self._on_remove)
         self.framework.observe(self.on.update_status, self._on_update_status)
+        self.framework.observe(self.on["mysql"].relation_joined, self._on_mysql_relation)
+        self.framework.observe(self.on["mysql"].relation_changed, self._on_mysql_relation)
+        self.framework.observe(self.on["mysql"].relation_departed, self._on_mysql_relation_remove)
+        self.framework.observe(self.on["mysql"].relation_broken, self._on_mysql_relation_remove)
+        self.framework.observe(
+            self.on["relational-db"].relation_joined, self._on_relational_db_relation
+        )
+        self.framework.observe(
+            self.on["relational-db"].relation_changed, self._on_relational_db_relation
+        )
+        self.framework.observe(
+            self.on["relational-db"].relation_departed, self._on_relational_db_relation_remove
+        )
+        self.framework.observe(
+            self.on["relational-db"].relation_broken, self._on_relational_db_relation_remove
+        )
+
+        # setup relational database interface and observers
+        self.database = DatabaseRequires(
+            self, relation_name="relational-db", database_name=self._database_name
+        )
+        self.framework.observe(self.database.on.database_created, self._on_relational_db_relation)
+        self.framework.observe(self.database.on.endpoints_changed, self._on_relational_db_relation)
 
         port = ServicePort(int(self._port), name="api")
         self.service_patcher = KubernetesServicePatch(
@@ -90,12 +115,12 @@ class KatibDBManagerOperator(CharmBase):
     def service_environment(self):
         """Return environment variables based on model configuration."""
         ret_env_vars = {
-            "DB_NAME": "mysql",
-            "DB_USER": "root",
-            "DB_PASSWORD": self._mysql_data["root_password"],
-            "KATIB_MYSQL_DB_HOST": self._mysql_data["host"],
-            "KATIB_MYSQL_DB_PORT": self._mysql_data["port"],
-            "KATIB_MYSQL_DB_DATABASE": self._mysql_data["database"],
+            "DB_NAME": self._db_data["db_name"],
+            "DB_USER": self._db_data["db_username"],
+            "DB_PASSWORD": self._db_data["db_password"],
+            "KATIB_MYSQL_DB_HOST": self._db_data["katib_db_host"],
+            "KATIB_MYSQL_DB_PORT": self._db_data["katib_db_port"],
+            "KATIB_MYSQL_DB_DATABASE": self._db_data["katib_db_name"],
         }
 
         return ret_env_vars
@@ -136,17 +161,169 @@ class KatibDBManagerOperator(CharmBase):
             self.logger.info("Not a leader, skipping setup")
             raise ErrorWithStatus("Waiting for leadership", WaitingStatus)
 
-    def _check_mysql(self):
+    def _on_mysql_relation(self, event):
+        """Check for existing database relations and process mysql relation if needed."""
+        # check for relational-db relation
+        # relying on KeyError to ensure that relational-db relation is not present
         try:
-            relation = self.model.relations["mysql"][0]
-            unit = next(iter(relation.units))
-            mysql_data = relation.data[unit]
-            # Ensure we've got some data sent over the relation
-            mysql_data["root_password"]
-        except (IndexError, StopIteration, KeyError):
-            raise ErrorWithStatus("Waiting for mysql connection information", WaitingStatus)
+            relation = self.model.get_relation("relational-db")
+            if relation:
+                self.logger.warning(
+                    "Up-to-date database relation relational-db is already established."
+                )
+                self.logger.error(f"{MYSQL_WARNING} {UNBLOCK_MESSAGE}")
+                self.model.unit.status = BlockedStatus(f"{UNBLOCK_MESSAGE} See logs")
+                return
+        except KeyError:
+            pass
+        # relational-db relation was not found, proceed with warnings
+        self.logger.warning(MYSQL_WARNING)
+        self.model.unit.status = MaintenanceStatus(f"Adding mysql relation. {MYSQL_WARNING}")
+        self._on_event(event)
 
-        return mysql_data
+    def _on_mysql_relation_remove(self, event):
+        """Process removal of mysql relation."""
+        self.model.unit.status = MaintenanceStatus(f"Removing mysql relation. {MYSQL_WARNING}")
+        self._on_event(event)
+
+    def _on_relational_db_relation(self, event):
+        """Check for existing database relations and process relational-db relation if needed."""
+        # relying on KeyError to ensure that mysql relation is not present
+        try:
+            relation = self.model.get_relation("mysql")
+            if relation:
+                self.logger.warning(
+                    "Failed to create relational-db relation due to existing mysql relation."
+                )
+                self.logger.error(f"{MYSQL_WARNING} {UNBLOCK_MESSAGE}")
+                self.model.unit.status = BlockedStatus(f"{UNBLOCK_MESSAGE} See logs")
+                return
+        except KeyError:
+            pass
+        # mysql relation was not found, proceed
+        self.model.unit.status = MaintenanceStatus("Adding relational-db relation")
+        self._on_event(event)
+
+    def _on_relational_db_relation_remove(self, event):
+        """Process removal of relational-db relation."""
+        self.model.unit.status = MaintenanceStatus("Removing relational-db relation")
+        self._on_event(event)
+
+    def _get_db_relation(self, relation_name):
+        """Retrieves relation with supplied relation name, if it is established.
+
+        Returns relation, if it is established, and raises error otherwise."""
+
+        try:
+            # retrieve relation data
+            relation = self.model.get_relation(relation_name)
+        except KeyError:
+            # relation was not found
+            relation = None
+        if not relation:
+            # relation is not established, raise an error
+            raise GenericCharmRuntimeError(
+                f"Database relation {relation_name} is not established or empty"
+            )
+
+        return relation
+
+    def _get_mysql_data(self) -> dict:
+        """Check mysql relation, retrieve and return data, if available."""
+        db_data = {}
+        relation_data = {}
+        relation = self._get_db_relation("mysql")
+
+        # retrieve database data from relation
+        try:
+            unit = next(iter(relation.units))
+            relation_data = relation.data[unit]
+            # retrieve database data from relation data
+            # this also validates the expected data by means of KeyError exception
+            db_data["db_name"] = self._database_name
+            db_data["db_username"] = relation_data["user"]
+            db_data["db_password"] = relation_data["root_password"]
+            db_data["katib_db_host"] = relation_data["host"]
+            db_data["katib_db_port"] = relation_data["port"]
+            db_data["katib_db_name"] = relation_data["database"]
+        except (IndexError, StopIteration, KeyError) as err:
+            # failed to retrieve database configuration
+            if not relation_data:
+                raise GenericCharmRuntimeError(
+                    "Database relation mysql is not established or empty"
+                )
+            self.logger.error(f"Missing attribute {err} in mysql relation data")
+            # incorrect/incomplete data can be found in mysql relation which can be resolved:
+            # use WaitingStatus
+            raise ErrorWithStatus(
+                "Incorrect/incomplete data found in relation mysql. See logs", WaitingStatus
+            )
+
+        return db_data
+
+    def _get_relational_db_data(self) -> dict:
+        """Check relational-db relation, retrieve and return data, if available."""
+        db_data = {}
+        relation_data = {}
+
+        # ignore return value, because data is retrieved from library
+        self._get_db_relation("relational-db")
+
+        # retrieve database data from library
+        relation_data = self.database.fetch_relation_data()
+        # parse data in relation
+        # this also validates expected data by means of KeyError exception
+        for val in relation_data.values():
+            if not val:
+                continue
+            try:
+                db_data["db_name"] = self._database_name
+                db_data["db_username"] = val["username"]
+                db_data["db_password"] = val["password"]
+                host, port = val["endpoints"].split(":")
+                db_data["katib_db_host"] = host
+                db_data["katib_db_port"] = port
+                db_data["katib_db_name"] = "database"
+            except KeyError as err:
+                self.logger.error(f"Missing attribute {err} in relational-db relation data")
+                # incorrect/incomplete data can be found in mysql relation which can be
+                # resolved: use WaitingStatus
+                raise ErrorWithStatus(
+                    "Incorrect/incomplete data found in relation relational-db. See logs",
+                    WaitingStatus,
+                )
+        # report if there was no data populated
+        if not db_data:
+            self.logger.info("Found empty relation data for relational-db relation.")
+            raise ErrorWithStatus("Waiting for relational-db data", WaitingStatus)
+
+        return db_data
+
+    def _get_db_data(self) -> dict:
+        """Check for MySQL relations -  mysql or relational-db - and retrieve data.
+
+        Only one database relation can be established at a time.
+        """
+        db_data = {}
+        try:
+            db_data = self._get_mysql_data()
+        except ErrorWithStatus as err:
+            # mysql relation is established, but data could not be retrieved
+            raise err
+        except GenericCharmRuntimeError:
+            # mysql relation is not established, proceed to check for relational-db relation
+            try:
+                db_data = self._get_relational_db_data()
+            except ErrorWithStatus as err:
+                # relation-db relation is established, but data could not be retrieved
+                raise err
+            except GenericCharmRuntimeError:
+                # mysql and relational-db relations are not established, raise error
+                raise ErrorWithStatus(
+                    "Please add required database relation: eg. relational-db", BlockedStatus
+                )
+
+        return db_data
 
     def _check_and_report_k8s_conflict(self, error):
         """Return True if error status code is 409 (conflict), False otherwise."""
@@ -182,7 +359,7 @@ class KatibDBManagerOperator(CharmBase):
         return self.container.get_check("katib-db-manager-up").status
 
     def _refresh_status(self):
-        """Check leader, refresh status of workload, and set status accordingly."""
+        """Check leader, refresh status of workload, and raise error if workload is unhealthy."""
         self._check_leader()
         try:
             check = self._get_check_status()
@@ -195,11 +372,12 @@ class KatibDBManagerOperator(CharmBase):
                 f"Container {self._container_name} failed health check. It will be restarted."
             )
             raise ErrorWithStatus("Workload failed health check", MaintenanceStatus)
-        else:
-            self.model.unit.status = ActiveStatus()
 
     def _on_update_status(self, event):
         """Update status actions."""
+        # skip update status processing in case of BlockedStatus
+        if isinstance(self.model.unit.status, BlockedStatus):
+            return
         try:
             self._refresh_status()
         except ErrorWithStatus as err:
@@ -234,7 +412,7 @@ class KatibDBManagerOperator(CharmBase):
         try:
             self._check_leader()
             self._apply_k8s_resources(force_conflicts=force_conflicts)
-            self._mysql_data = self._check_mysql()
+            self._db_data = self._get_db_data()
             update_layer(
                 self._container_name,
                 self._container,
--- charms/katib-db-manager/tests/integration/test_charm.py
+++ charms/katib-db-manager/tests/integration/test_charm.py
@@ -3,6 +3,8 @@ from pathlib import Path
 
 import pytest
 import yaml
+from charmed_kubeflow_chisme.exceptions import ErrorWithStatus
+from ops.model import RelationNotFoundError
 from pytest_operator.plugin import OpsTest
 
 logger = logging.getLogger(__name__)
@@ -13,31 +15,149 @@ APP_NAME = "katib-db-manager"
 KATIB_DB_CONFIG = {"database": "katib"}
 
 
-@pytest.mark.abort_on_fail
-async def test_build_and_deploy(ops_test: OpsTest):
-    """Build and deploy the charm.
-    Assert on the unit status.
-    """
-    charm_under_test = await ops_test.build_charm(".")
-    logger.info(f"Built charm {charm_under_test}")
-    image_path = METADATA["resources"]["oci-image"]["upstream-source"]
-    resources = {"oci-image": image_path}
+class TestCharm:
+    @pytest.mark.abort_on_fail
+    async def test_build_and_deploy(self, ops_test: OpsTest):
+        """Build and deploy the charm.
+        Assert on the unit status.
+        """
+        charm_under_test = await ops_test.build_charm(".")
+        logger.info(f"Built charm {charm_under_test}")
+        image_path = METADATA["resources"]["oci-image"]["upstream-source"]
+        resources = {"oci-image": image_path}
 
-    await ops_test.model.deploy(
-        charm_under_test, resources=resources, application_name=APP_NAME, trust=True
-    )
+        # set default series for test model
+        await ops_test.juju("model-config", "default-series=focal")
 
-    await ops_test.model.deploy(
-        entity_url="charmed-osm-mariadb-k8s", application_name="katib-db", config=KATIB_DB_CONFIG
-    )
-    await ops_test.model.add_relation(
-        f"{APP_NAME}:mysql",
-        "katib-db:mysql",
-    )
+        await ops_test.model.deploy(
+            charm_under_test, resources=resources, application_name=APP_NAME, trust=True
+        )
 
-    # NOTE: idle_period is used to ensure all resources are deployed
-    await ops_test.model.wait_for_idle(
-        apps=[APP_NAME], status="active", raise_on_blocked=True, timeout=60 * 10, idle_period=30
-    )
-    assert ops_test.model.applications[APP_NAME].units[0].workload_status == "active"
+        await ops_test.model.deploy(
+            entity_url="charmed-osm-mariadb-k8s",
+            application_name="katib-db",
+            config=KATIB_DB_CONFIG
+        )
+        await ops_test.model.wait_for_idle(
+            apps=["katib-db"],
+            status="active",
+            raise_on_blocked=False,
+            raise_on_error=False,
+            timeout=60 * 10,
+        )
+
+        await ops_test.model.wait_for_idle(
+            apps=[APP_NAME],
+            status="blocked",
+            raise_on_blocked=False,
+            raise_on_error=False,
+            timeout=60 * 10,
+        )
+
+        # deploy mysql-k8s charm
+        await ops_test.model.deploy("mysql-k8s", channel="8.0/stable", series="jammy", trust=True)
+        await ops_test.model.wait_for_idle(
+            apps=["mysql-k8s"],
+            status="active",
+            raise_on_blocked=True,
+            timeout=60 * 10,
+            idle_period=60
+        )
+
+        # test no database relation, charm should be in blocked state
+        assert ops_test.model.applications[APP_NAME].units[0].workload_status == "blocked"
+
+        # add mysql relation and verify that charm reaches active state
+        # NOTE: idle_period is used to ensure that relation data is propagated
+        await ops_test.model.relate(f"{APP_NAME}:mysql", "katib-db:mysql")
+        await ops_test.model.wait_for_idle(
+            apps=[APP_NAME, "katib-db"],
+            status="active",
+            raise_on_blocked=False,
+            raise_on_error=False,
+            timeout=60 * 10,
+            idle_period=60,
+        )
+        assert ops_test.model.applications[APP_NAME].units[0].workload_status == "active"
+
+    @pytest.mark.abort_on_fail
+    async def test_relational_db_relation_with_mysql_relation(self, ops_test: OpsTest):
+        """Test failure of addition of relational-db relation with mysql relation present."""
+
+        # add relational-db relation which should put charm into blocked state,
+        # because at this point mysql relation is already established
+        await ops_test.model.relate(f"{APP_NAME}:relational-db", "mysql-k8s:database")
+
+        # verify that charm goes into blocked state
+        await ops_test.model.wait_for_idle(
+            apps=[APP_NAME],
+            status="blocked",
+            raise_on_blocked=False,
+            raise_on_error=True,
+            timeout=60 * 10,
+            idle_period=10,
+        )
+        assert ops_test.model.applications[APP_NAME].units[0].workload_status == "blocked"
+
+        # remove just added relational-db relation
+        await ops_test.juju("remove-relation", f"{APP_NAME}:relational-db", "mysql-k8s:database")
+
+    @pytest.mark.abort_on_fail
+    async def test_relational_db_relation_with_mysql_k8s(self, ops_test: OpsTest):
+        """Test no relation and relation with mysql-k8s charm."""
+
+        # verify that charm is active state
+        await ops_test.model.wait_for_idle(
+            apps=[APP_NAME],
+            status="active",
+            raise_on_blocked=False,
+            raise_on_error=True,
+            timeout=60 * 10,
+        )
+        assert ops_test.model.applications[APP_NAME].units[0].workload_status == "active"
+
+        # remove existing mysql relation which should put charm into blocked state,
+        # because there will be no database relations
+        await ops_test.juju("remove-relation", f"{APP_NAME}:mysql", "katib-db:mysql")
+
+        # verify that charm goes into blocked state
+        await ops_test.model.wait_for_idle(
+            apps=[APP_NAME],
+            status="blocked",
+            raise_on_blocked=False,
+            raise_on_error=True,
+            timeout=60 * 10,
+        )
+        assert ops_test.model.applications[APP_NAME].units[0].workload_status == "blocked"
+
+        # add relational-db relation which should put charm into active state
+        await ops_test.model.relate(f"{APP_NAME}:relational-db", "mysql-k8s:database")
+
+        # verify that charm goes into active state
+        await ops_test.model.wait_for_idle(
+            apps=[APP_NAME],
+            status="active",
+            raise_on_blocked=False,
+            raise_on_error=True,
+            timeout=60 * 10,
+        )
+        assert ops_test.model.applications[APP_NAME].units[0].workload_status == "active"
+
+    @pytest.mark.abort_on_fail
+    async def test_msql_relation_with_relational_db_relation(self, ops_test: OpsTest):
+        """Test failure of addition of mysql relation with relation-db relation present."""
+
+        # add mysql relation which should put charm into blocked state,
+        # because at this point relational-db relation is already established
+        await ops_test.model.relate(f"{APP_NAME}:mysql", "katib-db:mysql")
+
+        # verify that charm goes into blocked state
+        await ops_test.model.wait_for_idle(
+            apps=[APP_NAME],
+            status="blocked",
+            raise_on_blocked=False,
+            raise_on_error=True,
+            timeout=60 * 10,
+        )
+        assert ops_test.model.applications[APP_NAME].units[0].workload_status == "blocked"
 
