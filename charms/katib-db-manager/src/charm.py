8e65d0c4d494f3db063084e2149dad4cd1b9f38f
i-chvets <113444075+i-chvets@users.noreply.github.com>
2023-04-24 19:28:20 -0400

    fix: remove defer and add wait skip (#77)
    Summary of changes:
    - Added call to main event handler to unblocked the charm.

--- charms/katib-db-manager/src/charm.py
+++ charms/katib-db-manager/src/charm.py
@@ -375,9 +375,12 @@ class KatibDBManagerOperator(CharmBase):
 
     def _on_update_status(self, event):
         """Update status actions."""
-        # skip update status processing in case of BlockedStatus
-        if isinstance(self.model.unit.status, BlockedStatus):
-            return
+        # execute main eevent handler case of BlockedStatus or WaitingStatus
+        # WaitingStatus assumes DB relation is empty/incorrect which prevents workload from
+        # starting
+        status = self.model.unit.status
+        if isinstance(status, BlockedStatus) or isinstance(status, WaitingStatus):
+            self._on_event(event)
         try:
             self._refresh_status()
         except ErrorWithStatus as err:
@@ -409,9 +412,6 @@ class KatibDBManagerOperator(CharmBase):
             force_conflicts (bool): Should only be used when need to resolved conflicts on K8S
                                     resources.
         """
-        if not self.container.can_connect():
-            event.defer()
-            return
         try:
             self._check_leader()
             self._apply_k8s_resources(force_conflicts=force_conflicts)
