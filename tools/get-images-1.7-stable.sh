bfaff7c32283c96937f046465ce663106cc077b2
Ivan Chvets <ivan.chvets@canonical.com>
2023-07-11 18:10:03 -0400

    feat: added sort unique

--- tools/get-images-1.7-stable.sh
+++ tools/get-images-1.7-stable.sh
@@ -9,13 +9,13 @@ STATIC_IMAGE_LIST=(
 # switch to branch for 1.7/stable
 git checkout origin/track/0.15
 IMAGE_LIST=()
-IMAGE_LIST+=($(grep image charms/katib-controller/src/suggestion.json | awk  '{print $2}'))
-IMAGE_LIST+=($(grep image charms/katib-controller/src/suggestion.json | awk  '{print $2}'))
-IMAGE_LIST+=($(grep image charms/katib-controller/src/enasCPUTemplate.yaml | awk  '{print $2}'))
-IMAGE_LIST+=($(grep image charms/katib-controller/src/early-stopping.json | awk  '{print $2}'))
-IMAGE_LIST+=($(grep image charms/katib-controller/src/defaultTrialTemplate.yaml | awk  '{print $2}'))
-IMAGE_LIST+=($(grep image charms/katib-controller/src/pytorchJobTemplate.yaml | awk  '{print $2}'))
-IMAGE_LIST+=($(grep image charms/katib-controller/src/metrics-collector-sidecar.json | awk  '{print $2}'))
+IMAGE_LIST+=($(grep image charms/katib-controller/src/suggestion.json | awk  '{print $2}' | sort --unique))
+IMAGE_LIST+=($(grep image charms/katib-controller/src/suggestion.json | awk  '{print $2}' | sort --unique))
+IMAGE_LIST+=($(grep image charms/katib-controller/src/enasCPUTemplate.yaml | awk  '{print $2}' | sort --unique))
+IMAGE_LIST+=($(grep image charms/katib-controller/src/early-stopping.json | awk  '{print $2}' | sort --unique))
+IMAGE_LIST+=($(grep image charms/katib-controller/src/defaultTrialTemplate.yaml | awk  '{print $2}' | sort --unique))
+IMAGE_LIST+=($(grep image charms/katib-controller/src/pytorchJobTemplate.yaml | awk  '{print $2}' | sort --unique))
+IMAGE_LIST+=($(grep image charms/katib-controller/src/metrics-collector-sidecar.json | awk  '{print $2}' | sort --unique))
 
 printf "%s\n" "${STATIC_IMAGE_LIST[@]}"
 printf "%s\n" "${IMAGE_LIST[@]}"
