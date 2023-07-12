25c01a0e98c892ff04a94c925b7e210b28fe7e5c
Ivan Chvets <ivan.chvets@canonical.com>
2023-07-12 13:25:39 -0400

    feat: added metadata images scan

--- tools/get-images-1.7-stable.sh
+++ tools/get-images-1.7-stable.sh
@@ -9,13 +9,14 @@ STATIC_IMAGE_LIST=(
 # switch to branch for 1.7/stable
 git checkout origin/track/0.15
 IMAGE_LIST=()
-IMAGE_LIST+=($(grep image charms/katib-controller/src/suggestion.json | awk  '{print $2}' | sort --unique))
-IMAGE_LIST+=($(grep image charms/katib-controller/src/suggestion.json | awk  '{print $2}' | sort --unique))
-IMAGE_LIST+=($(grep image charms/katib-controller/src/enasCPUTemplate.yaml | awk  '{print $2}' | sort --unique))
-IMAGE_LIST+=($(grep image charms/katib-controller/src/early-stopping.json | awk  '{print $2}' | sort --unique))
-IMAGE_LIST+=($(grep image charms/katib-controller/src/defaultTrialTemplate.yaml | awk  '{print $2}' | sort --unique))
-IMAGE_LIST+=($(grep image charms/katib-controller/src/pytorchJobTemplate.yaml | awk  '{print $2}' | sort --unique))
-IMAGE_LIST+=($(grep image charms/katib-controller/src/metrics-collector-sidecar.json | awk  '{print $2}' | sort --unique))
+IMAGE_LIST+=($(find $REPO -type f -name metadata.yaml -exec yq '.resources | to_entries | .[] | .value | ."upstream-source"' {} \;))
+IMAGE_LIST+=($(grep image charms/katib-controller/src/suggestion.json | awk  '{print $2}' | sort --unique | sed s/\"//g))
+IMAGE_LIST+=($(grep image charms/katib-controller/src/suggestion.json | awk  '{print $2}' | sort --unique | sed s/\"//g))
+IMAGE_LIST+=($(grep image charms/katib-controller/src/enasCPUTemplate.yaml | awk  '{print $2}' | sort --unique | sed s/\"//g))
+IMAGE_LIST+=($(grep image charms/katib-controller/src/early-stopping.json | awk  '{print $2}' | sort --unique | sed s/\"//g))
+IMAGE_LIST+=($(grep image charms/katib-controller/src/defaultTrialTemplate.yaml | awk  '{print $2}' | sort --unique | sed s/\"//g))
+IMAGE_LIST+=($(grep image charms/katib-controller/src/pytorchJobTemplate.yaml | awk  '{print $2}' | sort --unique | sed s/\"//g))
+IMAGE_LIST+=($(grep image charms/katib-controller/src/metrics-collector-sidecar.json | awk  '{print $2}' | sort --unique | sed s/\"//g | sed s/,//g))
 
 printf "%s\n" "${STATIC_IMAGE_LIST[@]}"
 printf "%s\n" "${IMAGE_LIST[@]}"
