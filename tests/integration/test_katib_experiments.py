7d624fbe6638386093d90437d30b3316f57523ba
NohaIhab <noha.ihab@canonical.com>
2023-08-08 14:57:48 +0000

    fix: remove test deploy from test_katib_experiments.py fix: add license

--- tests/integration/test_katib_experiments.py
+++ tests/integration/test_katib_experiments.py
@@ -1,4 +1,8 @@
-"""Integration tests for Katib Experiments."""
+# Copyright 2023 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""Integration tests for Katib Experiments.
+"""
 
 import logging
 from pathlib import Path
@@ -17,68 +21,6 @@ from utils import (
 )
 
 logger = logging.getLogger(__name__)
-
-
-CONTROLLER_PATH = Path("charms/katib-controller")
-UI_PATH = Path("charms/katib-ui")
-DB_PATH = Path("charms/katib-db-manager")
-
-CONTROLLER_METADATA = yaml.safe_load(Path(f"{CONTROLLER_PATH}/metadata.yaml").read_text())
-UI_METADATA = yaml.safe_load(Path(f"{UI_PATH}/metadata.yaml").read_text())
-DB_METADATA = yaml.safe_load(Path(f"{DB_PATH}/metadata.yaml").read_text())
-
-CONTROLLER_APP_NAME = CONTROLLER_METADATA["name"]
-UI_APP_NAME = UI_METADATA["name"]
-DB_APP_NAME = DB_METADATA["name"]
-
-logger = logging.getLogger(__name__)
-
-
-@pytest.mark.skip_if_deployed
-@pytest.mark.abort_on_fail
-async def test_deploy_katib_charms(ops_test: OpsTest):
-    # Build katib-controller, katib-db-manager, and katib-ui charms
-    controller_charm = await ops_test.build_charm(CONTROLLER_PATH)
-    db_manager_charm = await ops_test.build_charm(DB_PATH)
-    ui_charm = await ops_test.build_charm(UI_PATH)
-
-    # Gather metadata
-    controller_image_path = CONTROLLER_METADATA["resources"]["oci-image"]["upstream-source"]
-    db_image_path = DB_METADATA["resources"]["oci-image"]["upstream-source"]
-    ui_image_path = UI_METADATA["resources"]["oci-image"]["upstream-source"]
-
-    # Deploy katib-controller, katib-db-manager, and katib-ui charms
-    await ops_test.model.deploy(controller_charm, resources={"oci-image": controller_image_path})
-
-    await ops_test.model.deploy(
-        db_manager_charm, resources={"oci-image": db_image_path}, trust=True
-    )
-
-    await ops_test.model.deploy(ui_charm, resources={"oci-image": ui_image_path}, trust=True)
-
-    # Deploy katib-db
-    await ops_test.model.deploy(
-        "charmed-osm-mariadb-k8s",
-        application_name="katib-db",
-        config={"database": "katib"},
-    )
-
-    # Relate to katib-db
-    await ops_test.model.add_relation("katib-db-manager", "katib-db")
-
-    await ops_test.model.wait_for_idle(
-        status="active",
-        raise_on_blocked=False,
-        timeout=90 * 10,
-    )
-
-    # Wait for everything to deploy
-    await ops_test.model.wait_for_idle(
-        status="active",
-        raise_on_blocked=True,
-        timeout=360,
-    )
-
 
 @pytest.mark.parametrize(
     "experiment_file",
@@ -100,6 +42,7 @@ async def test_kaitb_experiments(ops_test: OpsTest, experiment_file):
 
     Each experiment is created, running, and has running trials.
     At the end, experiment is deleted.
+    NOTE: This test is re-using deployment created in test_deploy_katib_charms() in test_charms.py
     """
     namespace = ops_test.model_name
     lightkube_client = lightkube.Client()
--- tests/integration/utils.py
+++ tests/integration/utils.py
@@ -1,3 +1,6 @@
+# Copyright 2023 Canonical Ltd.
+# See LICENSE file for licensing details.
+
 import tenacity
 import yaml
 from lightkube import ApiError
