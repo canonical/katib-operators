19b2616472dd546e875df781299cf391f19904ec
Robert Gildein <robert.gildein@canonical.com>
2024-06-17 15:11:09 +0200

    fix integrations test for katib-db-manager
    Fix integrations tests and add them to CI.

--- charms/katib-db-manager/tests/integration/test_charm.py
+++ charms/katib-db-manager/tests/integration/test_charm.py
@@ -37,11 +37,20 @@ class TestCharm:
         await ops_test.model.deploy(
             charm_under_test, resources=resources, application_name=APP_NAME, trust=True
         )
+        await ops_test.model.wait_for_idle(
+            apps=[APP_NAME],
+            status="blocked",
+            raise_on_blocked=False,
+            raise_on_error=False,
+            timeout=60 * 10,
+        )
 
+        # deploy charmed-osm-mariadb-k8s charm
         await ops_test.model.deploy(
             entity_url="charmed-osm-mariadb-k8s",
             application_name="katib-db",
-            config=KATIB_DB_CONFIG
+            config=KATIB_DB_CONFIG,
+            trust=True
         )
         await ops_test.model.wait_for_idle(
             apps=["katib-db"],
@@ -63,9 +72,17 @@ class TestCharm:
         # test no database relation, charm should be in blocked state
         assert ops_test.model.applications[APP_NAME].units[0].workload_status == "blocked"
 
+        # Deploying grafana-agent-k8s and add all relations
+        await deploy_and_assert_grafana_agent(
+            ops_test.model, APP_NAME, metrics=False, dashboard=False, logging=True
+        )
+
+    @pytest.mark.abort_on_fail
+    async def test_relational_mysql(self, ops_test: OpsTest):
+        """Test adding mysql relation."""
         # add mysql relation and verify that charm reaches active state
         # NOTE: idle_period is used to ensure that relation data is propagated
-        await ops_test.model.relate(f"{APP_NAME}:mysql", "katib-db:mysql")
+        await ops_test.model.integrate(f"{APP_NAME}:mysql", "katib-db:mysql")
         await ops_test.model.wait_for_idle(
             apps=[APP_NAME, "katib-db"],
             status="active",
@@ -76,18 +93,13 @@ class TestCharm:
         )
         assert ops_test.model.applications[APP_NAME].units[0].workload_status == "active"
 
-        # Deploying grafana-agent-k8s and add all relations
-        await deploy_and_assert_grafana_agent(
-            ops_test.model, APP_NAME, metrics=False, dashboard=False, logging=True
-        )
-
     @pytest.mark.abort_on_fail
     async def test_relational_db_relation_with_mysql_relation(self, ops_test: OpsTest):
         """Test failure of addition of relational-db relation with mysql relation present."""
 
         # add relational-db relation which should put charm into blocked state,
         # because at this point mysql relation is already established
-        await ops_test.model.relate(f"{APP_NAME}:relational-db", "mysql-k8s:database")
+        await ops_test.model.integrate(f"{APP_NAME}:relational-db", "mysql-k8s:database")
 
         # verify that charm goes into blocked state
         await ops_test.model.wait_for_idle(
@@ -132,7 +144,7 @@ class TestCharm:
         assert ops_test.model.applications[APP_NAME].units[0].workload_status == "blocked"
 
         # add relational-db relation which should put charm into active state
-        await ops_test.model.relate(f"{APP_NAME}:relational-db", "mysql-k8s:database")
+        await ops_test.model.integrate(f"{APP_NAME}:relational-db", "mysql-k8s:database")
 
         # verify that charm goes into active state
         await ops_test.model.wait_for_idle(
@@ -150,7 +162,7 @@ class TestCharm:
 
         # add mysql relation which should put charm into blocked state,
         # because at this point relational-db relation is already established
-        await ops_test.model.relate(f"{APP_NAME}:mysql", "katib-db:mysql")
+        await ops_test.model.integrate(f"{APP_NAME}:mysql", "katib-db:mysql")
 
         # verify that charm goes into blocked state
         await ops_test.model.wait_for_idle(
