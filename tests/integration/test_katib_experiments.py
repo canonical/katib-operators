9d360642e32c2da2ddc04fae8d00c9d439c405ad
mvlassis <manos.vlassis@canonical.com>
2025-09-08 17:16:47 +0200

    Don't include simple-pbt.yaml in tests

--- tests/integration/test_katib_experiments.py
+++ tests/integration/test_katib_experiments.py
@@ -71,8 +71,11 @@ def create_profile(lightkube_client):
 
 @pytest.mark.parametrize(
     "experiment_file",
-    # Don't include simple-pbt.yaml because Canonical K8s doesn't support ReadWriteMany AccessMode
+    # Don't include simple-pbt.yaml by default as Canonical K8s doesn't support PVCs with
+    # ReadWriteMany AccessMode.
     # See: https://github.com/canonical/katib-operators/issues/347
+    # To include simple-pbt.yaml, replace the line below the comments with:
+    # glob.glob("tests/assets/crs/experiments/*.yaml"),
     [f for f in glob.glob("tests/assets/crs/experiments/*.yaml") if "simple-pbt.yaml" not in f],
 )
 async def test_katib_experiments(
