02257bfbb9e68f3c35c86aa5f06a1d0fced9818d
Dario Faccin <dario.faccin@canonical.com>
2025-12-09 11:11:10 +0100

    feat(katib-ui): run workload and charm as unprivileged user
    Signed-off-by: Dario Faccin <dario.faccin@canonical.com>

--- charms/katib-ui/tests/integration/test_charm_ambient.py
+++ charms/katib-ui/tests/integration/test_charm_ambient.py
@@ -9,14 +9,26 @@ import pytest
 import yaml
 from charmed_kubeflow_chisme.testing import (
     assert_path_reachable_through_ingress,
+    assert_security_context,
     deploy_and_integrate_service_mesh_charms,
+    generate_container_securitycontext_map,
+    get_pod_names,
 )
+from lightkube import Client
 from pytest_operator.plugin import OpsTest
 
-APP_NAME = "katib-ui"
 EXPECTED_RESPONSE_TEXT = "Frontend"
 HTTP_PATH = "/katib/"
 METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
+APP_NAME = METADATA["name"]
+CONTAINERS_SECURITY_CONTEXT_MAP = generate_container_securitycontext_map(METADATA)
+
+
+@pytest.fixture(scope="session")
+def lightkube_client() -> Client:
+    """Returns lightkube Kubernetes client"""
+    client = Client(field_manager=f"{APP_NAME}")
+    return client
 
 
 @pytest.mark.skip_if_deployed
@@ -65,4 +77,25 @@ async def test_ui_is_accessible(ops_test: OpsTest):
         namespace=ops_test.model_name,
         expected_content_type="text/html",
         expected_response_text=EXPECTED_RESPONSE_TEXT,
+    )
+
+
+@pytest.mark.parametrize("container_name", list(CONTAINERS_SECURITY_CONTEXT_MAP.keys()))
+async def test_container_security_context(
+    ops_test: OpsTest,
+    lightkube_client: Client,
+    container_name: str,
+):
+    """Test container security context is correctly set.
+
+    Verify that container spec defines the security context with correct
+    user ID and group ID.
+    """
+    pod_name = get_pod_names(ops_test.model.name, APP_NAME)[0]
+    assert_security_context(
+        lightkube_client,
+        pod_name,
+        container_name,
+        CONTAINERS_SECURITY_CONTEXT_MAP,
+        ops_test.model.name,
     )
