4622292ce670afcbe1995e3031f4d361037fcb4c
Dario Faccin <dario.faccin@canonical.com>
2025-12-16 18:03:59 +0100

    chore(katib-controller): remove hardcoded config path
    Signed-off-by: Dario Faccin <dario.faccin@canonical.com>

--- charms/katib-controller/src/charm.py
+++ charms/katib-controller/src/charm.py
@@ -49,9 +49,8 @@ K8S_RESOURCE_FILES = [
 ]
 
 KATIB_WEBHOOK_PORT = 8443
-CERTS_FOLDER = "/tmp/cert"
+CERTS_FOLDER = Path("/tmp/cert")
 KATIB_CONFIG_FILE = Path("src/templates/katib-config.yaml.j2")
-KATIB_CONFIG_DESTINATION_PATH = "/katib-config/katib-config.yaml"
 
 logger = logging.getLogger(__name__)
 
@@ -94,6 +93,12 @@ class KatibControllerOperator(CharmBase):
         super().__init__(*args)
 
         self._namespace = self.model.name
+
+        # Storage
+        self._container_name = next(iter(self.meta.containers))
+        _container_meta = self.meta.containers[self._container_name]
+        _storage_name = next(iter(_container_meta.mounts))
+        self._storage_path = Path(_container_meta.mounts[_storage_name].location)
 
         # Expose controller's ports
         webhook_port = ServicePort(
@@ -176,19 +181,19 @@ class KatibControllerOperator(CharmBase):
                 files_to_push=[
                     ContainerFileTemplate(
                         source_template_path=key_file.name,
-                        destination_path=f"{CERTS_FOLDER}/tls.key",
+                        destination_path=CERTS_FOLDER / "tls.key",
                     ),
                     ContainerFileTemplate(
                         source_template_path=cert_file.name,
-                        destination_path=f"{CERTS_FOLDER}/tls.crt",
+                        destination_path=CERTS_FOLDER / "tls.crt",
                     ),
                     ContainerFileTemplate(
                         source_template_path=ca_file.name,
-                        destination_path=f"{CERTS_FOLDER}/ca.crt",
+                        destination_path=CERTS_FOLDER / "ca.crt",
                     ),
                     ContainerFileTemplate(
                         source_template_path=KATIB_CONFIG_FILE,
-                        destination_path=KATIB_CONFIG_DESTINATION_PATH,
+                        destination_path=self._storage_path / "katib-config.yaml",
                         context_function=self._katib_config_context,
                     ),
                 ],
