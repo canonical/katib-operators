9c595db23cf1e157578726120187a59ed4cde7c3
mvlassis <manos.vlassis@canonical.com>
2026-01-09 14:26:26 +0200

    feat: update images

--- tools/get-images.sh
+++ tools/get-images.sh
@@ -6,4 +6,27 @@
 IMAGE_LIST=()
 IMAGE_LIST+=($(find . -type f -name metadata.yaml -exec yq '.resources | to_entries | .[] | .value | ."upstream-source"' {} \;))
 IMAGE_LIST+=($(yq '.[]' ./charms/katib-controller/src/default-custom-images.json  | sed 's/"//g'))
-printf "%s\n" "${IMAGE_LIST[@]}"
+
+# Exclude images for the trial templates
+EXCLUDED_IMAGES=(
+    "ghcr.io/kubeflow/katib/pytorch-mnist-cpu:v0.19.0"
+    "ghcr.io/kubeflow/katib/enas-cnn-cifar10-cpu:v0.19.0"
+)
+
+# Filter the list
+FILTERED_LIST=()
+for img in "${IMAGE_LIST[@]}"; do
+    skip=false
+    for exclude in "${EXCLUDED_IMAGES[@]}"; do
+        if [[ "$img" == "$exclude" ]]; then
+            skip=true
+            break
+        fi
+    done
+
+    if [[ "$skip" == "false" ]]; then
+        FILTERED_LIST+=("$img")
+    fi
+done
+
+printf "%s\n" "${FILTERED_LIST[@]}"
